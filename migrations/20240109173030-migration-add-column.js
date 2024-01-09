113fc5a18730a2c654a038944adca3d80493fc8e
Phioria <chris.baugh@gmail.com>
2024-01-09 12:35:59 -0500

    Add migration to add library_size column to database

--- migrations/20240109173030-migration-add-column.js
+++ migrations/20240109173030-migration-add-column.js
@@ -1,14 +1,12 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-/*
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.renameColumn('records', 'total_mapped', 'number_mapped');
+        await queryInterface.addColumn('records', 'library_size', Sequelize.INTEGER);
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.renameColumn('records', 'number_mapped', 'total_mapped');
+        await queryInterface.removeColumn('records', 'library_size');
     },
 };
-*/
