776798a664c5665e6d880f637eb3c55109e38bf9
Phioria <chris.baugh@gmail.com>
2024-01-09 11:48:43 -0500

    Change table from record to records
    Postgres pluralizes table names from submitted models

--- migrations/20240109162550-migration-column-rename.js
+++ migrations/20240109162550-migration-column-rename.js
@@ -3,10 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.renameColumn('record', 'total_mapped', 'number_mapped');
+        await queryInterface.renameColumn('records', 'total_mapped', 'number_mapped');
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.renameColumn('record', 'number_mapped', 'total_mapped');
+        await queryInterface.renameColumn('records', 'number_mapped', 'total_mapped');
     },
 };
